36f7fccc232ac0613d6b2f615814a8f4e4655daa
pikarestiv <okwyodumodu@gmail.com>
2024-05-15 07:25:02 +0100

    Add more routes

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../components/pages/Homepage";
+import Login from "../components/pages/Login";
+import SignUp from "../components/pages/SignUp";
 
 const AppRoutes: FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
       </Routes>
     </Router>
   );
