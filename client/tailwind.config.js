77c70700d8bb20e259898cb08117f813246ef57b
pikarestiv <okwyodumodu@gmail.com>
2024-05-15 11:23:33 +0100

    Add screens intailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "320px", // Extra small screens, 320px and up
+        sm: "480px", // Small screens, 480px and up
+        md: "768px", // Medium screens, 768px and up
+        lg: "1024px", // Large screens, 1024px and up
+        xl: "1200px", // Extra large screens, 1200px and up
+        "2xl": "1440px", // 2X large screens, 1440px and up
+      },
+    },
   },
   plugins: [],
 };
