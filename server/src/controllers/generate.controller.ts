e5a4d9507c103a9b3f86a327ca0ce63e87ea4a3f
pikarestiv <okwyodumodu@gmail.com>
2024-05-16 11:27:08 +0100

    Cleanup and change endpoint

--- server/src/controllers/generate.controller.ts
+++ server/src/controllers/generate.controller.ts
@@ -1,10 +1,8 @@
-import generateImageService, {
-  translate,
-} from '../services/generate.service.ts';
-import { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import generateImageService from "../services/generate.service.ts";
+import { Request, Response } from "express";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
 const apiKey = process.env.API_KEY;
 const apiEndpoint = process.env.API_ENDPOINT;
@@ -14,7 +12,12 @@ export const generateImage = async (
   res: Response
 ): Promise<any> => {
   const { prompt } = req.body;
-  console.log('Prompt', prompt);
+  // console.log('Prompt', prompt);
+  console.log("data", {
+    apiKey,
+    apiEndpoint,
+  });
+
   try {
     const ImageData = await generateImageService(prompt, {
       apiKey,
@@ -26,22 +29,3 @@ export const generateImage = async (
   }
 };
 
-export const translateText = async (
-  req: Request,
-  res: Response
-): Promise<any> => {
-  try {
-    const { text } = req.body;
-    if (!text)
-      return res
-        .status(400)
-        .json({ success: false, message: 'Text cannot be empty' });
-    const translatedText = await translate(text);
-    res.json({
-      message: 'Text translated',
-      data: { originalText: text, translatedText },
-    });
-  } catch (error: any) {
-    res.status(400).json({ success: false, message: error.message });
-  }
-};
