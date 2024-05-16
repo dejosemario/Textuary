4688f989eb08f70e85118e0e41a2f94ffa66b14d
pikarestiv <okwyodumodu@gmail.com>
2024-05-16 17:18:43 +0100

    Fix gradient

--- client/src/components/atoms/CustomAvatar.tsx
+++ client/src/components/atoms/CustomAvatar.tsx
@@ -2,8 +2,15 @@ import { FC } from "react";
 const CustomAvatar: FC<CustomAvatarProps> = ({ src, size = 32, onClick }) => {
   return (
     <div
-      className={`relative border border-transparent bg-gradient-to-r from-[#FBDA61] to-[#FF5ACD] rounded-full`}
-      style={{ width: size, height: size }}
+      className={`relative rounded-full cursor-pointer`}
+      style={{
+        width: size,
+        height: size,
+        border: "1px solid transparent",
+        background:
+          "linear-gradient(#090600, #090600) padding-box, linear-gradient(109.15deg, #FBDA61 0%, #FF5ACD 100%) border-box",
+        borderRadius: "50%",
+      }}
       onClick={onClick}
     >
       <div className="w-full h-full bg-[#090600] rounded-full flex items-center justify-center overflow-hidden">
