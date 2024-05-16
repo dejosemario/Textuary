459c222165be9fdb2dd8e9d857434b5d4b13aff6
pikarestiv <okwyodumodu@gmail.com>
2024-05-16 19:20:07 +0100

    Make Logo responsive

--- client/src/components/atoms/Logo.tsx
+++ client/src/components/atoms/Logo.tsx
@@ -1,13 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import logoText from "../../assets/logo-text.svg";
+import logo from "../../assets/logo-40x40.svg";
 
 const Logo = ({ url = "/" }) => {
   let navigate = useNavigate();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const logoSrc = isMobile ? logo : logoText;
 
   return (
     <img
       className="cursor-pointer"
-      src={logoText}
+      src={logoSrc}
       onClick={() => navigate(url)}
       alt="logo"
     />
