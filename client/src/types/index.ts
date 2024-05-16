a8042b81ca5684167f2614a689c1e2fe20370930
pikarestiv <okwyodumodu@gmail.com>
2024-05-16 17:19:05 +0100

    Add types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,8 +1,22 @@
+export type ChatMessage = {
+  sender: "user" | "model";
+  content: string;
+  timestamp: Date;
+  imageUrl?: string | null | undefined;
+};
+export type ChatHistory = {
+  user: string;
+  title: string;
+  messages: ChatMessage[];
+  startedAt: Date;
+  updatedAt: Date;
+};
+
 export type ChatData = {
   loading: boolean;
   chatActive: boolean;
   chatBotMessage: any;
   currentConversationId: "";
-  messagesList: any[];
+  messagesList: ChatMessage[];
   chatHistory: any;
 };
