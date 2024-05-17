41fb9982fbc0a1ee3bc3aa20d28a5bb34535fa0a
pikarestiv <okwyodumodu@gmail.com>
2024-05-17 15:40:19 +0100

    Cleanup

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -4,7 +4,6 @@ import {
   Routes,
   Route,
   Navigate,
-  useNavigate,
 } from "react-router-dom";
 import HomePage from "../components/pages/Homepage";
 import Login from "../components/pages/Login";
